306a390738bbd96f4a86dcef14a5ca2b188c0e98
Karkuvel Prabakaran <66421441+karkuvel05@users.noreply.github.com>
2024-01-02 20:37:38 +0530

    Update script.js

--- Engineer_Cafe/JS/script.js
+++ Engineer_Cafe/JS/script.js
@@ -10,3 +10,39 @@ window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 };
+
+
+
+//Image 
+function changeContent(category) {
+    // Hide all containers
+    var containers = document.querySelectorAll('.container');
+
+    containers.forEach(function(container) {
+        container.style.display = 'none';
+    });
+
+    
+
+    // Show the selected container
+    var selectedContainer = document.getElementById(category);
+    if (selectedContainer) {
+        selectedContainer.style.display = 'grid';
+    }
+    
+}
+
+//Login Popup
+var modal = document.getElementById('login');
+window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+}
+var modal1 = document.getElementById('signup');
+window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+}
+
